52f8101b990f7df4d611b98a8fa9b691518fd29b
KunalTakke <kunal.technophilic@gmail.com>
2025-05-12 13:50:09 +0530

    feat: add program for variable and operators

---
+++ 01-Basics/Basics/src/com/basics/java/VariablesOperators.java
@@ -0,0 +1,30 @@
+package com.basics.java;
+
+public class VariablesOperators {
+
+	public static void main(String[] args) {
+		
+		// Addition 
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("Addition is "+c);
+
+		// Subtraction 
+		c = b -a  ;
+		System.out.println("Subtraction is "+c);
+		
+		// Multiplication 
+		c = a*b;
+		System.out.println("Multiplication is "+c);
+		
+		// Division
+		c = b/a;
+		System.out.println("Division is "+c);
+		
+		//Remainder
+		c = b%a;
+		System.out.println("Remainder is "+c);
+	}
+
+}
