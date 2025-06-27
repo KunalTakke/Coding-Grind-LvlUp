03551f5ae7b261bb87d0818beb20341a8b74d26b
KunalTakke <kunal.technophilic@gmail.com>
2025-06-27 18:26:01 +0530

    Feat: Add Program for Upper Bound
    Program to find the upper bound of an element in a sorted array

---
+++ 02-Arrays/Arrays/src/com/singleDimension/java/UpperBoundSortedArray.java
@@ -0,0 +1,40 @@
+package com.singleDimension.java;
+
+public class UpperBoundSortedArray {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 3, 4 };
+		int ele = 3;
+		int res = upperBoundSortedArray(arr, ele);
+
+		System.out.println("The upper bound index for " + ele + " is " + res);
+
+	}
+
+	public static int upperBoundSortedArray(int[] arr, int element) {
+
+		int low = 0;
+		int high = arr.length - 1;
+		int ans = -1;
+
+		while (low <= high) {
+
+			int mid = (low + high) / 2;
+
+			if (arr[mid] > element) {
+				low = mid + 1;
+			} else if (arr[mid] < element) {
+				high = mid - 1;
+			} else { // arr[mid] == element
+				ans = mid;
+				low = mid + 1;
+			}
+
+		}
+
+		return ans;
+
+	}
+
+}
